8b08b210b7ca6fcee1c16f77ce76dc8ac2badc43
mp30028 <mp30028@gmail.com>
2022-05-23 07:25:24 +0100

    added code to try out Flux.create

--- src/main/java/com/zonesoft/addressbook/controllers/DummyServerSentEventController.java
+++ src/main/java/com/zonesoft/addressbook/controllers/DummyServerSentEventController.java
@@ -5,24 +5,24 @@ import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
 @RestController
 public class DummyServerSentEventController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DummyServerSentEventController.class);
 
-	@GetMapping(path = "/sse/test-with-generate", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public Flux<DummyEntity> sseTestWithGenerate() {
-		LOGGER.debug("[SSE-CONTROLLER] sseTestWithGenerate");
+	@GetMapping(path = "/sse/tryout-with-create", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<DummyEntity> sseTryoutWithCreate() {
+		LOGGER.debug("[SSE-CONTROLLER] sseTryoutWithCreate");
 
-		Flux<DummyEntity> acaciaFlux = Flux.generate((sink) -> {
+		Flux<DummyEntity> dummyFlux = Flux.create((FluxSink<DummyEntity> sink) -> {
 			Random random = new Random();
 			String name = generateName();
 			int a = random.nextInt(101);
@@ -34,12 +34,31 @@ public class DummyServerSentEventController {
 			DummyEntity dummyEntity = new DummyEntity(name, a, b, now);
 			sink.next(dummyEntity);
 		});
-		return acaciaFlux;
+		return dummyFlux;
 	}
 
-	@GetMapping(path = "/sse/test-with-interval", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public Flux<DummyEntity> sseTestWithInterval() {
-		LOGGER.debug("[SSE-CONTROLLER] sseTestWithInterval");
+	@GetMapping(path = "/sse/tryout-with-generate", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<DummyEntity> sseTryoutWithGenerate() {
+		LOGGER.debug("[SSE-CONTROLLER] sseTryoutWithGenerate");
+
+		Flux<DummyEntity> dummyFlux = Flux.generate((sink) -> {
+			Random random = new Random();
+			String name = generateName();
+			int a = random.nextInt(101);
+			int b = random.nextInt(101);
+			Date now = new Date();
+			int waitSeconds = randomInt(10, 20);
+			LOGGER.debug("name = {}, valueA= {}, valueB={}, date={}", name, a, b, now);
+			this.wait(waitSeconds, "[WAIT-sseTestWithGenerate]");
+			DummyEntity dummyEntity = new DummyEntity(name, a, b, now);
+			sink.next(dummyEntity);
+		});
+		return dummyFlux;
+	}
+
+	@GetMapping(path = "/sse/tryout-with-interval", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<DummyEntity> sseTryouWithInterval() {
+		LOGGER.debug("[SSE-CONTROLLER] sseTryouWithInterval");
 		Random random = new Random();
 		return Flux.interval(Duration.ofSeconds(1))
 				.map(it -> new DummyEntity(generateName(), random.nextInt(101), random.nextInt(101), new Date()));
