30299a0c9f5011e42f66095ab05473c59db36d4c
mp30028 <mp30028@gmail.com>
2022-06-19 02:32:43 +0100

    Other Names edit done

--- frontend/src/components/OtherNamesEdit.js
+++ frontend/src/components/OtherNamesEdit.js
@@ -6,7 +6,7 @@ function OtherNamesEdit(props){
 	const FUNCTION = " FUNCTION:OtherNamesEdit";
 	const emptyOtherNames = [];
 	const [currentOtherNames, setCurrentOtherNames] = useState(emptyOtherNames);
-	
+	const [selectedOtherName, setSelectedOtherName] = useState(null);
 	useEffect(
 		() => {
 			setCurrentOtherNames(props.otherNames);
@@ -36,30 +36,75 @@ function OtherNamesEdit(props){
 		let newOtherNames =[];
 		newOtherNames = currentOtherNames.map(on => { return ((on.id === otherName.id) ? otherName : on) });
 		setCurrentOtherNames(newOtherNames);	
+	};
+	
+	const isOtherNameChecked = (id) =>{
+		const ROUTINE = "ROUTINE:isOtherNamChecked";
+		console.log(`${MODULE} ${FUNCTION} ${ROUTINE} id=`, id, "selectedOtherName.id=" , selectedOtherName ? selectedOtherName.id : "selectedOtherName is null" );
+		return ( selectedOtherName ? selectedOtherName.id === id : false);
 	}
+	
+	const handleSelection = (event) => {
+		const ROUTINE = "ROUTINE:handleSelection";
+		const idToFind = parseInt(event.target.value);
+		const targetOtherName = currentOtherNames.find(on => on.id === idToFind);
+		console.log(`${MODULE} ${FUNCTION} ${ROUTINE} targetOtherName=`, targetOtherName);
+		if (targetOtherName){
+			setSelectedOtherName(targetOtherName);	
+		}else{
+			setSelectedOtherName(null);
+		}
+	}
+	
+	const handleSubmit = (event) =>{
+		event.preventDefault();
+//		switch(event.target.value){
+//			case "UPDATE":
+//				selectedPerson.otherNames = selectedPersonsOtherNames;
+//				props.updatePerson(selectedPerson);
+//				break;
+//			case "CREATE":
+//				selectedPerson.otherNames = selectedPersonsOtherNames;
+//				props.updatePerson(selectedPerson);
+//				break;
+//			case "CANCEL":
+//				navigate("/list", { replace: true });
+//				break;
+//			case "DELETE":
+//				navigate("/list", { replace: true });
+//				props.deletePerson(selectedPerson);
+//				break;
+//			default:
+//				navigate("/list", { replace: true });
+//		}
+	};	
 	
 		return(
 			<div style={{width: "100%"}}>
 				<table>
 					<thead>
 						<tr>
-							<th style={{ width: "50%" }}>Other Name</th>
-							<th style={{ width: "50%" }}>Type</th>
+							<th style={{ width: "40%" }}>Other Name</th>
+							<th style={{ width: "40%" }}>Type</th>
+							<th></th>
 						</tr>
 					</thead>
 					<tbody>
-						{currentOtherNames ?
-							currentOtherNames.map(otherName =>
+						{currentOtherNames.map(otherName =>
 							<tr key={otherName.id}>
 								<td><input type="text" name="otherNameValue" id={"value_" + otherName.id} value={otherName.value} onChange={updateOtherName}/></td>
 								<td><input type="text" name="otherNameType" id={"otherNameType.value_" + otherName.id}  value={otherName.otherNameType.value} onChange={updateOtherName}/></td>
-							</tr>
-							
-						):
+								<td style={{textAlign:"center"}}>
+									<input type="radio" name="selectOtherName" id={'selectOtherName_' + otherName.id} value={otherName.id} onChange={handleSelection} checked={isOtherNameChecked(otherName.id)} />
+									<label htmlFor={'selectOtherName_' + otherName.id} className="ellipses">. . .</label>
+								</td>
+							</tr>	
+						)}
 						<tr>
-							<td colSpan="2" style={{textAlign: "center"}}> -- No Other Names -- </td>
+							<td colSpan="3" style={{textAlign:"right"}}>
+								<button type="submit" onClick={handleSubmit} value="CREATE">Add</button>
+							</td>
 						</tr>
-						}
 					</tbody>
 				</table>
 			</div>
