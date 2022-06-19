6fe5b0d487a2cbdf636c1b7fbe669a466111b38c
mp30028 <mp30028@gmail.com>
2022-06-19 10:17:31 +0100

    remove commented code and clean up in OtherNameEdit

--- frontend/src/components/OtherNamesEdit.js
+++ frontend/src/components/OtherNamesEdit.js
@@ -6,8 +6,7 @@ function OtherNamesEdit(props){
 	const FUNCTION = " FUNCTION:OtherNamesEdit";
 	const emptyOtherNames = [];
 	const [otherNames, setOtherNames] = useState(emptyOtherNames);
-
-//	const [selectedOtherName, setSelectedOtherName] = useState(null);
+	
 	useEffect(
 		() => {
 			setOtherNames(props.otherNames);
@@ -39,24 +38,6 @@ function OtherNamesEdit(props){
 		setOtherNames(newOtherNames);	
 	};
 	
-//	const isOtherNameChecked = (id) =>{
-//		const ROUTINE = "ROUTINE:isOtherNamChecked";
-//		console.log(`${MODULE} ${FUNCTION} ${ROUTINE} id=`, id, "selectedOtherName.id=" , selectedOtherName ? selectedOtherName.id : "selectedOtherName is null" );
-//		return ( selectedOtherName ? selectedOtherName.id === id : false);
-//	}
-	
-//	const handleSelection = (event) => {
-//		const ROUTINE = "ROUTINE:handleSelection";
-//		const idToFind = parseInt(event.target.value);
-//		const targetOtherName = currentOtherNames.find(on => on.id === idToFind);
-//		console.log(`${MODULE} ${FUNCTION} ${ROUTINE} targetOtherName=`, targetOtherName);
-//		if (targetOtherName){
-//			setSelectedOtherName(targetOtherName);	
-//		}else{
-//			setSelectedOtherName(null);
-//		}
-//	}
-//	
 	const handleDeleteOtherName = (event) =>{
 		const ROUTINE = "FUNCTION: handleDeleteOtherName";
 		console.log(`${MODULE} ${FUNCTION} ${ROUTINE} event.target.name=`, event.target.name, "event.target.value=", event.target.value, "event.target.id=", event.target.id);
@@ -64,28 +45,7 @@ function OtherNamesEdit(props){
 		const targetId = parseInt(event.target.value);
 		const newOtherNames = otherNames.filter((on) => { return (on.id === targetId ? null : on) });
 		setOtherNames(newOtherNames);
-
-//		switch(event.target.value){
-//			case "UPDATE":
-//				selectedPerson.otherNames = selectedPersonsOtherNames;
-//				props.updatePerson(selectedPerson);
-//				break;
-//			case "CREATE":
-//				selectedPerson.otherNames = selectedPersonsOtherNames;
-//				props.updatePerson(selectedPerson);
-//				break;
-//			case "CANCEL":
-//				navigate("/list", { replace: true });
-//				break;
-//			case "DELETE":
-//				navigate("/list", { replace: true });
-//				props.deletePerson(selectedPerson);
-//				break;
-//			default:
-//				navigate("/list", { replace: true });
-//		}
 	};	
-	
 	
 	const handleAddNewOtherName = (event) =>{
 		const ROUTINE = "FUNCTION: handleAddNewOtherName";
@@ -93,12 +53,9 @@ function OtherNamesEdit(props){
 		const temporaryId = Date.now() * -1;
 		const emptyOtherName = {id: temporaryId , value: "", otherNameType: {id: 6, value: "Firstname at birth"}};
 		event.preventDefault();
-//		const targetId = parseInt(event.target.value);
 		const newOtherNames = [...otherNames, emptyOtherName];
 		setOtherNames(newOtherNames);
 	}
-	
-	
 	
 		return(
 			<div style={{width: "100%"}}>
@@ -117,11 +74,6 @@ function OtherNamesEdit(props){
 								<td><input type="text" name="otherNameType" id={"otherNameType.value_" + otherName.id}  value={otherName.otherNameType.value} onChange={handleChangeToOtherName}/></td>
 								<td style={{textAlign:"center"}}>
 								<button className="deleteButton" name="deleteOtherName" id={'deleteOtherName_' + otherName.id} value={otherName.id}  onClick={handleDeleteOtherName}/>
-								
-{/*								
-									<input type="radio" name="selectOtherName" id={'selectOtherName_' + otherName.id} value={otherName.id} onChange={handleSelection} checked={isOtherNameChecked(otherName.id)} />
-									<label htmlFor={'selectOtherName_' + otherName.id} className="ellipses">. . .</label>
- */}
 								</td>
 							</tr>	
 						)}
@@ -134,6 +86,8 @@ function OtherNamesEdit(props){
 				</table>
 			</div>
 		);
+		
+
 };
 
 export default OtherNamesEdit;
