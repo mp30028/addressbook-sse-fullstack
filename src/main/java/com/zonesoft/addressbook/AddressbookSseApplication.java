7367f9609a9d3078b389d055dbdb4384a2bd2076
mp30028 <mp30028@gmail.com>
2022-07-07 06:11:29 +0100

    routing fixed by enabling web mvc and adding default router

--- src/main/java/com/zonesoft/addressbook/AddressbookSseApplication.java
+++ src/main/java/com/zonesoft/addressbook/AddressbookSseApplication.java
@@ -2,13 +2,28 @@ package com.zonesoft.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.web.reactive.config.EnableWebFlux;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.RouterFunctions;
+//import org.springframework.web.reactive.function.server.ServerResponse;
 
 
 @SpringBootApplication
+//@EnableWebFlux
+//@Configuration
 public class AddressbookSseApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AddressbookSseApplication.class, args);
 	}
 	
+	
+//	@Bean
+//	RouterFunction<ServerResponse> staticResourceRouter(){
+//	    return RouterFunctions.resources("/**{path:[^\\\\.]*}", new ClassPathResource("forward:/"));
+////	    return RouterFunctions.resources("/**", new ClassPathResource("static/"));
+//	}
 }
--- src/main/java/com/zonesoft/addressbook/configurations/ApplicationStartupConfigs.java
+++ src/main/java/com/zonesoft/addressbook/configurations/ApplicationStartupConfigs.java
@@ -6,18 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.RouterFunctions;
+//import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.zonesoft.addressbook.events.PersistenceEventPublisher;
 
 @Configuration
-@EnableWebFlux
 public class ApplicationStartupConfigs {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationStartupConfigs.class);
 	
@@ -31,14 +29,15 @@ public class ApplicationStartupConfigs {
 	public WebFluxConfigurer corsConfigurer() {
 		return new WebFluxConfigurer() {
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:8081","http://localhost:3000" );
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080","http://localhost:3000" );
 //				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
 	
-	@Bean
-	RouterFunction<ServerResponse> staticResourceRouter(){
-	    return RouterFunctions.resources("/**", new ClassPathResource("static/"));
-	}
+//	@Bean
+//	RouterFunction<ServerResponse> staticResourceRouter(){
+//	    return RouterFunctions.resources("/**{path:[^\\\\.]*}", new ClassPathResource("forward:/"));
+////	    return RouterFunctions.resources("/**", new ClassPathResource("static/"));
+//	}
 }
